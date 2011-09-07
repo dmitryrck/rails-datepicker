6affda88859b661b7e32fba5abeb12f674adcc70
Dmitry Nix <dmitry.nix@gmail.com>
2011-09-07 08:21:25 -0300

    Usando dia_br

--- app/models/aniversario.rb
+++ app/models/aniversario.rb
@@ -1,2 +1,9 @@
 class Aniversario < ActiveRecord::Base
+  def dia_br
+    "#{dia.to_s[8..9]}/#{dia.to_s[5..6]}/#{dia.to_s[0..3]}"
+  end
+
+  def dia_br=(dia_en)
+    "#{dia_en[6..9]}-#{dia_en[3..4]}-#{dia_en[0..1]}"
+  end
 end
